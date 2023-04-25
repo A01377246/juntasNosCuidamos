3158cbf416fb8558df01f25f5b65238bd9eb9ec6
Humberto Carrillo Gómez (hcarrill) <hcarrill@cisco.com>
2023-04-24 21:13:33 -0600

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Stylesheet, Text, View } from 'react-native'
+
+const HomeScreen = () =>{
+
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    )
+}
+
+export default HomeScreen
